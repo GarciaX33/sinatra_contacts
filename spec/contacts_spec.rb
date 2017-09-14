921ea48e8b3425372c8dbe529eedd762f9bed203
daniel garcia and gabrijela gavric <daniel@>
2017-09-14 10:28:37 -0700

    pass second rspec describing .all

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,6 +2,11 @@ require('pry')
 require('rspec')
 require('contacts')
 
+describe(".all") do
+it("is empty at first") do
+  expect(Contact.all()).to(eq([]))
+  end
+end
 describe('#save') do
   it("saves the users informatin and pushes into an array") do
     contact = Contact.new("Danny", "Garcia", "Student", "Epicodus", "400 SW 6th Avenue")
